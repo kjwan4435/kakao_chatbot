1cc3dfd8fbf50390c7d3bfa5fdc9c571f0ae268a
kjwan4435 <kjwan4435@gmail.com>
2020-08-05 19:48:35 +0900

    router api heroku

--- routes/api.js
+++ routes/api.js
@@ -1,20 +1,4 @@
-const express = require("express");
-const app = express();
-const PORT = process.env.PORT || 80;
-const logger = require("morgan");
-const bodyParser = require("body-parser");
-
 const apiRouter = express.Router();
-
-app.use(logger("dev", {}));
-app.use(bodyParser.json());
-app.use(
-  bodyParser.urlencoded({
-    extended: true
-  })
-);
-
-app.use("/api", apiRouter);
 
 apiRouter.post("/sayHello", function (req, res) {
   const responseBody = {
@@ -52,8 +36,4 @@ apiRouter.post("/showHello", function (req, res) {
   };
 
   res.status(200).send(responseBody);
-});
-
-app.listen(PORT, function () {
-  console.log("Example skill server listening on port!");
 });
---
+++ server.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 80;
+const logger = require("morgan");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const apiRouter = express.Router();
+
+const enterRouter = require("./routes/enter");
+const activityRouter = require("./routes/activity");
+const foodRouter = require("./routes/food");
+const moodRouter = require("./routes/mood");
+const sleepRouter = require("./routes/sleep");
+const socialRouter = require("./routes/social");
+
+app.use(cors());
+app.use(logger("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use("/api", apiRouter);
+
+apiRouter.post("/sayHello", function (req, res) {
+  const responseBody = {
+    version: "2.0",
+    template: {
+      outputs: [
+        {
+          simpleText: {
+            text: "hello I'm Ryan"
+          }
+        }
+      ]
+    }
+  };
+
+  res.status(200).send(responseBody);
+});
+
+apiRouter.post("/showHello", function (req, res) {
+  console.log(req.body);
+
+  const responseBody = {
+    version: "2.0",
+    template: {
+      outputs: [
+        {
+          simpleImage: {
+            imageUrl:
+              "https://t1.daumcdn.net/friends/prod/category/M001_friends_ryan2.jpg",
+            altText: "hello I'm Ryan"
+          }
+        }
+      ]
+    }
+  };
+
+  res.status(200).send(responseBody);
+});
+
+app.listen(PORT, function () {
+  console.log("Example skill server listening on port!");
+});
