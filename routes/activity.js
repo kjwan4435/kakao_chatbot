466661505d616b71a903055e305250c1d9d767b4
kjwan4435 <kjwan4435@gmail.com>
2020-08-05 20:01:20 +0900

    router api heroku2

---
+++ routes/activity.js
@@ -0,0 +1,41 @@
+const acitivityRouter = require("express").Router();
+
+acitivityRouter.post("/sayHello", function (req, res) {
+  const responseBody = {
+    version: "2.0",
+    template: {
+      outputs: [
+        {
+          simpleText: {
+            text: "hello I'm Ryan"
+          }
+        }
+      ]
+    }
+  };
+
+  res.status(200).send(responseBody);
+});
+
+acitivityRouter.post("/showHello", function (req, res) {
+  console.log(req.body);
+
+  const responseBody = {
+    version: "2.0",
+    template: {
+      outputs: [
+        {
+          simpleImage: {
+            imageUrl:
+              "https://t1.daumcdn.net/friends/prod/category/M001_friends_ryan2.jpg",
+            altText: "hello I'm Ryan"
+          }
+        }
+      ]
+    }
+  };
+
+  res.status(200).send(responseBody);
+});
+
+module.exports = acitivityRouter;
--- server.js
+++ server.js
@@ -1,10 +1,7 @@
 const express = require("express");
-const app = express();
 const logger = require("morgan");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-
-const PORT = process.env.PORT || 80;
 
 const apiRouter = require("./routes/api");
 const enterRouter = require("./routes/enter");
@@ -14,17 +11,20 @@ const moodRouter = require("./routes/mood");
 const sleepRouter = require("./routes/sleep");
 const socialRouter = require("./routes/social");
 
+const app = express();
+const PORT = process.env.PORT || 80;
+
 app.use(cors());
 app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use("/enter", enterRouter);
+// app.use("/enter", enterRouter);
 app.use("/activity", activityRouter);
-app.use("/food", foodRouter);
-app.use("/mood", moodRouter);
-app.use("/sleep", sleepRouter);
-app.use("/social", socialRouter);
+// app.use("/food", foodRouter);
+// app.use("/mood", moodRouter);
+// app.use("/sleep", sleepRouter);
+// app.use("/social", socialRouter);
 
 app.use("/api", apiRouter);
 
